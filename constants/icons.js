ed6717c0638adc0f60b2ae47f6a023bf493d7697
sergPOV <serg3yp4vlov495@gmail.com>
2024-12-09 00:18:50 +0700

    подчистил

--- constants/icons.js
+++ constants/icons.js
@@ -1,22 +1,12 @@
-import bookmark from "../assets/icons/bookmark.png";
 import vk from "../assets/icons/vk.png";
 import google from "../assets/icons/google.png";
 import user from "../assets/icons/user.png";
 import kalendar from "../assets/icons/kalendar.png";
 import doghouse from "../assets/icons/doghouse.png";
 import course from "../assets/icons/course.png";
-import home from "../assets/icons/home.png";
-import plus from "../assets/icons/plus.png";
-import profile from "../assets/icons/profile.png";
-import leftArrow from "../assets/icons/left-arrow.png";
-import menu from "../assets/icons/menu.png";
-import search from "../assets/icons/search.png";
-import upload from "../assets/icons/upload.png";
-import rightArrow from "../assets/icons/right-arrow.png";
-import logout from "../assets/icons/logout.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
-import play from "../assets/icons/play.png";
+
 
 export default {
   google,
@@ -25,17 +15,6 @@ export default {
   kalendar,
   course,
   doghouse,
-  play,
-  bookmark,
-  home,
-  plus,
-  profile,
-  leftArrow,
-  menu,
-  search,
-  upload,
-  rightArrow,
-  logout,
   eyeHide,
   eye,
 };
